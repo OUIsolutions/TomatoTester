1f7e29656f576cb4690ffbdd895aa798b0526394
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-05 06:52:31 -0300

    at

--- src/globals.js
+++ src/globals.js
@@ -2,6 +2,6 @@
 const TOMATO_DEFAULT_SEED = 'hello my cold friend';
 
 let  tomato_min_rgb = 50;
-let  tomato_max_rgb = 200;
-let  tomato_minimum_difference = 70;
+let  tomato_max_rgb = 230;
+let  tomato_minimum_difference = 60;
 let tomato_total_generations = 0;
