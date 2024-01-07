e52ac0c06b434c5814bc2c0c6b7ede56554fdbbd
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-06 22:05:25 -0300

    att

--- src/default_props.js
+++ src/default_props.js
@@ -2,4 +2,4 @@
 const TOMATO_DEFAULT_SEED = 'hello my cold friend';
 const  TOMATO_MIN_RGB = 60;
 const  TOMATO_MAX_RGB = 220;
-const  TOMATO_MAX_DIFFERENCE = 50;
+const  TOMATO_MIN_DIFFERENCE = 50;
--- src/props_constructor.js
+++ src/props_constructor.js
@@ -34,7 +34,7 @@ function tomato_construct_props(element){
     props.seed = get_value_or_default(props.seed,TOMATO_DEFAULT_SEED);
     props.min_rgb = get_value_or_default(props.min_rgb,TOMATO_MIN_RGB);
     props.max_rgb = get_value_or_default(props.max_rgb,TOMATO_MAX_RGB);
-    props.min_difference = get_value_or_default(props.min_difference,TOMATO_MAX_DIFFERENCE);
+    props.min_difference = get_value_or_default(props.min_difference,TOMATO_MIN_DIFFERENCE);
 
     return  props;
 }
