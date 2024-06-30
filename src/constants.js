6c14e6bb5897cef260f29fb00f81529da773aa46
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-06-30 10:40:17 -0300

    att

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
 const TOMATO_DEFAULT_SEED = 'hello my cold Friend';
-const  TOMATO_MIN_RGB = 20;
-const  TOMATO_MAX_RGB = 255;
+const  TOMATO_MIN_RGB = 60;
+const  TOMATO_MAX_RGB = 260;
 const TOTA_GENERATIONS = 100
